abbeb837a31577c72044a69a286273d1cf53c1f6
Matias Lucero <matiaslucero9815@gmail.com>
2022-05-05 23:02:41 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const controladoresApi = require('./controllers/controladoresApi.js');
+const controladoresWeb = require('./controllers/controladoresWeb.js');
+
+const routerApiProductos = require('./routers/routerApiProductos.js');
+
+const app = express();
+const PORT = 8080;
+//==============================
+app.use(routerApiProductos);
+
+
+const server = app.listen(PORT, () => {
+    console.log(`Escuchando en el puerto ${PORT}`);
+})
+server.on('error', error => {console.log(error.message)})
