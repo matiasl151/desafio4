62977492a5e360d697061d2f7dcfc3a5158ed6a3
Matias Lucero <matiaslucero9815@gmail.com>
2022-05-09 12:51:08 -0300

    metodos http implementados

--- controllers/controladoresApi.js
+++ controllers/controladoresApi.js
@@ -1,18 +1,11 @@
 const ContenedorArchivo = require('../databases/contenedorArchivo.js')
 const contenedor = new ContenedorArchivo('./databases/productos.txt')
 
-// TODO comparar estructura de body del request con la estructura de los productos
-// const estructura = {
-//     "title": "",
-//     "price": "",
-//     "thumbnail": ""
-// }
-
 const controladoresApi = {
 
     getProductos : async (req, res) => {
-        const prod = await contenedor.getAll()
-        res.json(prod)
+        const productos = await contenedor.getAll()
+        res.json(productos)
     },
     getProducto : async (req, res) => {
 
@@ -25,25 +18,31 @@ const controladoresApi = {
             res.json(producto)
         } catch (error) {
             res.status(500).json({ error: error.message })
-    
         }
     },
     postProducto : async (req, res) => {
-        // TODO agregar validacion si ya existe producto con ese id
+        if (!req.body.title || !req.body.price || !req.body.thumbnail){
+            res.status(400).json({ error: "Falta un atributo"})
+            return;
+        }
         req.body.id = Date.now()
         const productoAgregado = await contenedor.save(req.body)
         res.status(201).json(productoAgregado)
     },
     updateProducto : async (req, res) => {
-        // Esta medio agarrado de los pelos
         const idProducto = req.params.idProducto
         try{
             const producto = await contenedor.getById(idProducto)
             if (producto == undefined){
                 res.status(404).json({error: 'producto no encontrado'})
             }
+            if (!req.body.title || !req.body.price || !req.body.thumbnail){
+                res.status(400).json({ error: "Falta un atributo"})
+                return;
+            }
             await contenedor.deleteById(idProducto)
             req.body.id = idProducto
+            
             const productoActualizado = await contenedor.save(req.body)
             res.status(200).json(productoActualizado)
 
--- routers/routerApiProductos.js
+++ routers/routerApiProductos.js
@@ -8,15 +8,16 @@ const routerApiProductos = new Router()
 routerApiProductos.use(express.json())
 routerApiProductos.use(express.urlencoded({ extended: true }))
 
+// GET methods
 routerApiProductos.get('/', controladoresWeb.root);
 routerApiProductos.get('/inicio', controladoresWeb.inicio);
 routerApiProductos.get('/api/productos', controladoresApi.getProductos);
 routerApiProductos.get('/api/productos/:idProducto', controladoresApi.getProducto);
-
+// POST methods
 routerApiProductos.post('/api/productos/', controladoresApi.postProducto);
-
+// PUT methods
 routerApiProductos.put('/api/productos/:idProducto', controladoresApi.updateProducto);
-
+// DELETE methods
 routerApiProductos.delete('/api/productos/:idProducto', controladoresApi.deleteProducto);
 
 
